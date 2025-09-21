c1f47676fc5ebda26b6d5e56356951e9eaba6b11
Tarun Kumar <tarunkumarsingh295@gmailcom>
2025-09-21 21:23:34 +0530

    enables Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { inject } from "@vercel/analytics";
+
+inject(); // enables Vercel Analytics
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
